78706563bd8ebef780ac3640c86d034147196dfa
victor-avancini <victor.avancini@hotmail.com>
2023-10-16 15:40:56 -0300

    Tarefa concluída

--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -1,1 +1,22 @@
 /* Desenvolva seu código aqui... */
+
+function handleModal(){
+    const link = document.querySelector(".form__register-link")
+    const modalController = document.querySelector(".modalController")
+
+    link.addEventListener("click", function(){
+        modalController.showModal();
+        closeModal()
+    });
+}
+
+handleModal()
+
+function closeModal(){
+    const button = document.querySelector(".closeModal")
+    const modalController = document.querySelector(".modalController")
+
+    button.addEventListener("click", function(){
+        modalController.close();
+    })
+}
